5d0fe03bc51329558d5f23faec15a5f940dadfe0
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-04-18 20:27:24 +0600

    off plan / commercial (onhover event instead of onclick event) --- done

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const SearchField = () => {
+
+
     return (
         <form className='flex flex-col md:flex-row gap-4 justify-center items-center py-4'>
             <div className='flex justify-between items-center gap-1.5'>
-            <button className='cursor-pointer px-16 md:px-8 py-4 text-gray-400 bg-[#f6f6f1] rounded-md hover:border border-black'>Buy</button>
-            <button className='cursor-pointer px-16 md:px-8 py-4 text-gray-400 bg-[#f6f6f1] rounded-md hover:border border-black'>Rent</button>
+            <button onClick={(e) => e.preventDefault()} className='cursor-pointer px-16 md:px-8 py-4 text-gray-400 bg-[#f6f6f1] rounded-md hover:border border-black'>Buy</button>
+            <button onClick={(e) => e.preventDefault()} className='cursor-pointer px-16 md:px-8 py-4 text-gray-400 bg-[#f6f6f1] rounded-md hover:border border-black'>Rent</button>
             </div>
             <div className='flex flex-col md:flex-row justify-center items-center gap-4 text-gray-400 bg-gray-100 border border-gray-300  px-6 rounded-md'>
                 <select className=' px-8 py-4 text-gray-400 bg-gray-100 cursor-pointer'>
@@ -13,7 +15,7 @@ const SearchField = () => {
                     <option className='cursor-pointer' value="residential">Commercial</option>
                     <option className='cursor-pointer' value="offPlan">Off Plan</option>
                 </select>
-                <input type="text" placeholder='City, building or community' className='py-4 px-8' />
+                <input type="text" placeholder='City, building or community' className='py-4 px-8 focus:outline-none' />
             </div>
             <button type='submit' className='font-bold bg-green-300 text-gray-700 px-8 hover:text-white hover:bg-green-800 py-4 rounded-full cursor-pointer'>Search</button>
         </form>
