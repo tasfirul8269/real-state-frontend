f84b60ca08dd0eb8fd4fcb241701fe83a9ccb450
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-04-29 01:25:56 +0600

    Responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,14 +10,14 @@ import NewsAndInsights from "../../components/NewsAndInsights/NewsAndInsights";
 
 const Home = () => {
   return (
-    <div className="mb-10 px-5 md:px-0">
+    <div className="mb-10">
       <div className="md:relative">
         <Banner></Banner>
         <div className=" md:absolute md:-bottom-14 md:left-1/2 md:transform md:-translate-x-1/2  w-full max-w-4xl px-4 bg-white rounded-[20px] shadow-lg mx-auto p-4">
           <SearchField></SearchField>
         </div>
       </div>
-      <div className="px-0 md:px-[60px] mx-auto">
+      <div className=" md:px-[60px] px-5  mx-auto">
       <Services></Services>
       <FindConsultant></FindConsultant>
       <TopProperties></TopProperties>
