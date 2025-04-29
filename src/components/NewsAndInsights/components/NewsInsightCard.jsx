698d595569146bbeec4ab8ac0ab126b05823bf62
tasfirul8269 <adef05675@gmail.com>
2025-04-30 03:49:06 +0600

    Update NewsInsightCard.jsx

--- src/components/NewsAndInsights/components/NewsInsightCard.jsx
+++ src/components/NewsAndInsights/components/NewsInsightCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NewsInsightCard = ({newsInsight}) => {
     return (
-        <div>
+        <div class="border border-[#e6e6e6] rounded-[20px] p-[15px] ">
             <img className='w-full' src={newsInsight.image} alt="News Insight" />
             <h3 className='text-xl font-semibold mt-2'>{newsInsight.title}</h3>
             <p className='text-gray-600 mt-2'>{newsInsight.description}</p>
