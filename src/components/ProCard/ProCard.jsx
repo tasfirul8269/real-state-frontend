02a2dfd8179c381b954709f55793519e70a534e8
tasfirul8269 <adef05675@gmail.com>
2025-04-30 03:56:35 +0600

    Update ProCard.jsx

--- src/components/ProCard/ProCard.jsx
+++ src/components/ProCard/ProCard.jsx
@@ -16,78 +16,84 @@ import { LiaBathSolid, LiaBedSolid } from "react-icons/lia";
 const ProCard = ({ property }) => {
   console.log(property.beds);
   return (
-    <div className="p-4 my-5 md:my-0 border border-[#e6e6e6] rounded-[20px] animate__animated animate__fadeInUp]">
-      <img src={property?.image} alt={property?.name} className="rounded-[15px]" />
-      <div className="flex justify-between items-center my-4">
-        <h3 className="text-2xl font-semibold">
-          {property?.name.length > 15
-            ? property?.name.slice(0, 15) + "..."
-            : property?.name}
+    <div className="w-[395px] p-[15px] my-5 md:my-0 border border-[#e6e6e6] rounded-[20px] animate__animated animate__fadeInUp]">
+      <img src={property?.image} alt={property?.name} className="rounded-[15px] w-[380px] h-[214px]" />
+      <div className="flex justify-between items-center mt-4">
+        <h3 className="truncate text-[20px] font-medium">
+          {property?.name}
         </h3>
-        <button className="btn btn-outline outline-[#FFBF6A] bg-[#FFF5E7] btn-warning text-[#FF9B17]">
+        {property.propertyType =="Villa" ? <button className="py-[5px] px-[10px] ml-[10px] bg-[#FFF5E7] text-[#FF9B17] rounded-[10px]">
           {property?.propertyType}
-        </button>
+        </button> :  <button className="py-[5px] px-[10px] ml-[10px] bg-[#E8F7FF] text-[#256fff] rounded-[10px]">
+          {property?.propertyType}
+        </button>}
+       
       </div>
 
       {/* location container */}
       <div className="flex justify-start items-center gap-2">
         <img src={locationImg} alt={property?.location} />
-        <p className="text-gray-500">{property?.location}</p>
+        <p className="mt-[5px] text-[#999999] font-medium text-[14px]">{property?.location}</p>
       </div>
 
       {/* features container */}
-      <div className="flex justify-start gap-16 items-center my-4">
+      <div className="flex justify-start gap-[30px] items-center my-4">
         <div className="text-[#999999] flex justify-start items-center gap-3">
-          <LiaBedSolid className="text-2xl" />
-          <p className="text-xl">{property?.beds}</p>
+          <LiaBedSolid className="w-[20px] h-[20px] text-[2xl]" />
+          <p className="font-medium text-[16px]">{property?.beds}</p>
         </div>
+
+        <div class="h-3 w-[1.5px] bg-[#e6e6e6] mx-4"></div>
+
         <div className="text-[#999999] flex justify-start items-center gap-3">
-        <LiaBathSolid  className="text-2xl" />
-          <p className="text-xl">{property?.baths}</p>
+        <LiaBathSolid  className="w-[20px] h-[20px] text-2xl" />
+          <p className="font-medium text-[16px]">{property?.baths}</p>
         </div>
+
+        <div class="h-3 w-[1.5px] bg-[#e6e6e6] mx-4"></div>
+
+
         <div className="text-[#999999] flex justify-start items-center gap-3">
-          <img className="w-6  h-6" src={kitchen} alt="Kitchen" />
-          <p className="text-xl">{property?.kitchens}</p>
+         <img className="w-6  h-6" src={kitchen} alt="Kitchen" />
+          <p className="font-medium text-[16px]">{property?.kitchens}</p>
         </div>
       </div>
 
       {/* Agent container */}
       <div className="flex justify-between items-center my-4 gap-3 mb-5">
         <img
-            className="w-16 h-16 rounded-full"
+            className="w-13 h-13 rounded-full"
           src={property?.agentImage}
           alt={property?.agentName}
         />
 
         {/* agent name ana languages container */}
         <div>
-          <h3 className="text-md font-semibold">
+          <h3 className="text-md font-medium">
             {
-                property?.agentName.length > 8 ? property?.agentName.slice(0, 8) + "..." : property?.agentName
+                property?.agentName.split(" ")[0]
             }
           </h3>
-          <p className="text-sm text-gray-600">
-            Speaks {property?.languages?.join(", ")}
+          <p className="text-[12px] text-gray-600">
+            Speaks {property?.languages?.slice(0,2).join(", ")}
           </p>
         </div>
-        <h2 className="text-xl font-semibold">{property?.price}</h2>
+        <h2 className="text-[18px] font-medium">{property?.price}</h2>
       </div>
 
-      <img src={divider} className="my-5"/>
+      <div class="h-[1px] w-80% bg-[#e6e6e6] my-4"></div>
 
 
       {/* buttons container */}
       <div className="flex items-center justify-between">
-        <button  className="flex justify-center items-center gap-2 text-[#999999] bg-[#E6E6E6] px-4 py-3 rounded-md">
-          <FaPhone />
-          <span className="font-semibold">Call</span>
+        <button  className="w-[80px] flex justify-center items-center gap-2 text-[#8D8D8D] bg-[#E6E6E6] px-[10px] py-[10px] rounded-[10px]">
+          <span className="font-medium">Call</span>
         </button>
-        <button  className="flex justify-center items-center gap-2 text-[#00BD6E] bg-[#E5FFF1] px-4 py-3 rounded-md">
-          <FaWhatsapp />
-          <span className="font-semibold">Whatsapp</span>
+        <button  className="w[auto] flex justify-center items-center gap-2 text-[#00BD6E] bg-[#E5FFF1] px-[20px] py-[10px] rounded-[10px]">
+          <span className="font-medium">Whatsapp</span>
         </button>
-        <button  className="flex justify-center items-center gap-2 text-[#256FFF] bg-[#EBF8FF] px-4 py-3 rounded-md">
-          <span className="font-semibold">Booking</span>
+        <button  className="w-[auto] flex justify-center items-center gap-2 text-[#256FFF] bg-[#EBF8FF] px-[20px] py-[10px] rounded-[10px]">
+          <span className="font-medium">Book a view</span>
         </button>
       </div>
     </div>
