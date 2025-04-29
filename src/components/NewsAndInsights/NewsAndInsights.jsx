ed375f9bd93644dff8cdc7a5c0b9421abdebbeda
tasfirul8269 <adef05675@gmail.com>
2025-04-30 03:52:33 +0600

    Update NewsAndInsights.jsx

--- src/components/NewsAndInsights/NewsAndInsights.jsx
+++ src/components/NewsAndInsights/NewsAndInsights.jsx
@@ -37,12 +37,12 @@ const NewsAndInsights = () => {
     ]
 
     return (
-        <div className='bg-gray-100 py-20 md:py-40  sm:px-6 '>
-            <div className='max-w-7xl mx-auto'>
+        <div className=' py-20 md:py-10  '>
+            <div className=' mx-auto'>
                 {/* Header Section */}
                 <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-8 space-y-4 md:space-y-0 px-4'>
                     <h1 className='text-2xl md:text-3xl font-bold'>News and Insights</h1>
-                    <button className='text-blue-500 bg-blue-200 px-4 py-2 md:px-5 md:py-3 rounded-md text-sm md:text-base'>
+                    <button className='text-[#256fff] bg-[#E8F7FF] px-4 py-2 md:px-5 md:py-3 font-medium rounded-[10px] text-sm md:text-base'>
                         All News
                     </button>
                 </div>
