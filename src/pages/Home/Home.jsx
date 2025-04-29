0adb87a91166623653b705b192d28264c4fc2ccf
tasfirul8269 <adef05675@gmail.com>
2025-04-30 03:20:44 +0600

    Home.jsx
    Redesigned Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,44 +3,8 @@ import Banner from "../../components/Banner/Banner";
 import SearchField from "../../components/SearchField/SearchField";
 import Services from "../../components/Services/Services";
 import TopProperties from "../../components/TopProperties/TopProperties";
-import PropertySearchTabs from "../../components/PropertySearchByFeature/PropertySearchByFeature";
 import FindConsultant from "../../components/FindConsultant/FindConsultant";
-import PropertyValuation from "../../components/PropertyValuation/PropertyValuation";
 import NewsAndInsights from "../../components/NewsAndInsights/NewsAndInsights";
-import Frame from "../../components/LocationBasedProperty/locationCategory";
-import LocationCategory from "../../components/LocationBasedProperty/locationCategory";
-
-const Home = () => {
-  return (
-    <div className="mb-10">
-      <div className="md:relative">
-        <Banner></Banner>
-        <div className=" md:absolute md:-bottom-14 md:left-1/2 md:transform md:-translate-x-1/2  w-full max-w-4xl px-4 bg-white rounded-[20px] shadow-lg mx-auto p-4">
-          <SearchField></SearchField>
-        </div>
-      </div>
-      <div className=" md:px-[60px] px-5  mx-auto">
-      <Services></Services>
-      <FindConsultant></FindConsultant>
-      <TopProperties></TopProperties>
-      <LocationCategory></LocationCategory>
-      <NewsAndInsights></NewsAndInsights>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
-import React from "react";
-import Banner from "../../components/Banner/Banner";
-import SearchField from "../../components/SearchField/SearchField";
-import Services from "../../components/Services/Services";
-import TopProperties from "../../components/TopProperties/TopProperties";
-import PropertySearchTabs from "../../components/PropertySearchByFeature/PropertySearchByFeature";
-import FindConsultant from "../../components/FindConsultant/FindConsultant";
-import PropertyValuation from "../../components/PropertyValuation/PropertyValuation";
-import NewsAndInsights from "../../components/NewsAndInsights/NewsAndInsights";
-import Frame from "../../components/LocationBasedProperty/locationCategory";
 import LocationCategory from "../../components/LocationBasedProperty/locationCategory";
 
 const Home = () => {
